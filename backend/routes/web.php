f6596e3d9e1560185e88e73128ccccc15f4fd11a
Rodrigo Martins <rodrigotxt@gmail.com>
2026-07-06 01:58:11 -0300

    remove rotas de api das rotas publicas (web)

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,6 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Incluir rotas da API (má prática - misturando rotas web e api)
-require __DIR__.'/api.php';
