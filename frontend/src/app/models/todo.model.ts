4ecfcade627cfda296324c1581c80b33c78aaf9b
Rodrigo Martins <rodrigotxt@gmail.com>
2026-07-06 01:44:52 -0300

    criacao de model para Todo

---
+++ frontend/src/app/models/todo.model.ts
@@ -0,0 +1,11 @@
+export interface Todo {
+  id?: number;
+  title: string;
+  completed: boolean;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export interface CreateTodoDto {
+  title: string;
+}
